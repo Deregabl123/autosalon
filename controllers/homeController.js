b5459a1daa4d756c037e5213150147137939106f
Kuppuch <kuppuchr@gmail.com>
2022-05-19 23:59:38 +0300

    добавлена таблица и форма

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -15,3 +15,26 @@ exports.market = function (request, response) {
 exports.license = function (request, response) {
     response.render("license.hbs");
 };
+
+exports.form = function (request, response) {
+    response.render("form.hbs");
+};
+
+exports.addAuto = function (request, response) {
+    const name = request.body.name;
+    const model = request.body.model;
+    const color = request.body.color;
+    const price = request.body.price;
+
+    const roleId = 2;
+    db.auto.create({
+        name: name,
+        color: color,
+        model: model,
+        price: price
+    }).then(res=>{
+        const user = {id: res.id, name: res.name, color: res.color, model: res.model, price: res.price}
+        console.log(user);
+    }).catch(err=>console.log(err));
+    response.redirect("/market");
+};
--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -7,5 +7,8 @@ homeRouter.get("/about", homeController.about);
 homeRouter.get("/", homeController.index);
 homeRouter.get("/license", homeController.license);
 homeRouter.get("/market", homeController.market);
+homeRouter.get("/form", homeController.form);
+homeRouter.post("/form", homeController.addAuto);
+
 
 module.exports = homeRouter;
